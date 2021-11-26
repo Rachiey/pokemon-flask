c5c2bfa8c97e7fb0eb31b0b8fa7dce1342dab23b
“Rachiey” <rachaelyonoff@gmail.com>
2021-11-26 01:04:04 +0000

    trying to add forms

--- server.py
+++ server.py
@@ -1,14 +1,45 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, url_for, render_template, redirect
 from flask_cors import CORS
 from controllers import pokemon
 from werkzeug import exceptions
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 server = Flask(__name__)
+server.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
+Bootstrap(server)
 CORS(server)
+
+class NameForm(FlaskForm):
+    name = StringField('Which actor is your favorite?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+headings = ("Name", "Type")
+data = [
+    {'id': 1, 'name': 'Charizard', 'type': 'fire'},
+    {'id': 2, 'name': 'Bulbasaur', 'type': 'grass'},
+    {'id': 3, 'name': 'Squirtle', 'type': 'water'}
+]
 
 @server.route('/')
 def home():
-    return jsonify({'message': 'Hello from our Pokemon!'}), 200
+    
+    form = NameForm()
+    message = ""
+    if form.validate_on_submit():
+        name = form.name.data
+        if name.lower() in data:
+            # empty the form field
+            form.name.data = ""
+            id = data.id
+            # redirect the browser to another route and template
+            return redirect( url_for('pokemon', id=id) )
+        else:
+            message = "That actor is not in our database."
+    return render_template('home.html', data=data, form=form, message=message)
+    # return render_template("home.html", data=data, form = form )
 
 @server.route('/pokemon', methods=['GET', 'POST'])
 def pokemons_handler():
